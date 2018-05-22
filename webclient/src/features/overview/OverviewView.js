04f714dc89f998fda8a458cde569db37940846f0
Minh Nguyen <s4355206@student.uq.edu.au>
2018-05-22 20:51:16 +1000

    charts funcitoning with proper data

--- webclient/src/features/overview/OverviewView.js
+++ webclient/src/features/overview/OverviewView.js
@@ -11,65 +11,80 @@ export default class OverviewView extends React.Component {
     componentDidMount() {
         OverviewStore.getData();
     }
-
-    handleChange = (e, { value }) => {
-        OverviewStore.charts = value;
-        console.log(Array.apply(null, OverviewStore.charts));
-        
-
-        var testData = {
-            labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-            datasets: [{
-                label: '# of Votes',
-                data: [12, 19, 3, 5, 2, 3],
-                backgroundColor: [
-                    'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)',
-                    'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)',
-                    'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'
-                ],
-                borderWidth: 1
-            }]
-        }
-
-        var testOption = {
-            scales: {
-                yAxes: [{
-                    ticks: {
-                        beginAtZero: true
-                    }
-                }]
-            }
-        }
+    componentDidUpdate() {
+        const amounts = this.getAmounts();
+        const labels = this.getLabels();
+        console.log(amounts + labels);
 
         Object.values(this.refs).forEach(canvas => {
             let myChart = new Chart(canvas, {
                 type: canvas.id,
-                data: testData,
-                options: testOption
+                data: {
+                    labels: labels,
+                    datasets: [{
+                        label: '# of Votes',
+                        data: amounts,
+                        backgroundColor: [
+                            'rgba(255, 99, 132, 0.2)', 'rgba(54, 162, 235, 0.2)',
+                            'rgba(255, 206, 86, 0.2)', 'rgba(75, 192, 192, 0.2)',
+                            'rgba(153, 102, 255, 0.2)', 'rgba(255, 159, 64, 0.2)'
+                        ],
+                        borderWidth: 1
+                    }]
+                },
             });
         });
+    }
 
+    getLabels = () => {
+        var labels = [];
+        var data = OverviewStore.incomes.map(income => {
+            if (!labels.includes(income.description)) {
+                labels.push(income.description);
+            }
+        });
+        return labels;
+    }
+
+    getAmounts= () => {
+        var amounts = [];
+        var labels = this.getLabels();
+        labels.forEach(label => {
+            const results = OverviewStore.incomes.filter(income => label === income.description);
+            var sum = results.reduce((accumulator, currentValue) => {
+                return accumulator + currentValue.amount;
+            }, 0);
+            amounts.push(sum);
+        })
+        return amounts;
+    }
+
+
+    handleChange = (e, { value }) => {
+        OverviewStore.charts = value;
     }
 
     render() {
-        const chartOptions = [
-            { key: '1', value: 'pie', text: 'pie' },
-            { key: '2', value: 'radar', text: 'radar' },
-            { key: '3', value: 'line', text: 'line'},
-        ]; 
+
+        const chartOptions = OverviewStore.chartOptions.map((option, index) => {
+            return { key: index, value: option, text: option }
+        });
+        const selectedCharts = OverviewStore.charts.toJS();
 
         const charts = OverviewStore.charts.map((chart, index) => {
-            return <div><canvas ref={chart} key={index} id={chart} height={'200'} width={'300'}></canvas></div>
+            return <div key={index}>
+                <canvas ref={chart} id={chart} height={'200'} width={'300'}></canvas>
+            </div>
         });
-        
+
         return (
             <div>
-                <Dropdown multiple selection 
-                    options={chartOptions} 
-                    placeholder='Select your chart' 
+                <Dropdown multiple selection
+                    options={chartOptions}
+                    placeholder='Select your chart'
                     onChange={this.handleChange}
-                    value={Array.apply(null, OverviewStore.charts)}
-                /> 
+                    value={selectedCharts}
+                />
 
                 {charts}
 
